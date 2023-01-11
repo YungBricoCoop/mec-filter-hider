82a45edc82e33fa01d2d5bc72888a06b43c8e44d
Elwan <elwan.mayencourt@gmail.com>
2023-01-11 09:30:35 +0100

    feat : plugin header

---
+++ mec-filter-hider.php
@@ -0,0 +1,10 @@
+<?php
+/*
+ * Plugin Name: M.E.C. Filter Hider
+ * Plugin URI: https://github.com/YungBricoCoop/mec-filter-hider
+ * Description: Hide some filters on the Modern Events Calendar plugin
+ * Version: 1.0
+ * Author: Elwan Mayencourt
+ */
+
+ 
